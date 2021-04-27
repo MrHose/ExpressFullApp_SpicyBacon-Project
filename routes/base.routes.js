b3af8539605e9e605710758092ae67a1718c540b
Willy <willyman99@gmail.com>
2021-04-27 11:04:14 +0200

    Unfinished game

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const Game = require('./../models/game.model')
+
 // Endpoints
 router.get('/', (req, res) => {
     res.render('pages/index')
@@ -11,7 +13,12 @@ router.get('/auth', (req, res) => {
 })
 
 router.get('/game', (req, res) => {
-    res.render('pages/game/index')
+    Game
+        .create()
+        .then(createdGame => {
+            res.render('pages/game/index', createdGame)
+        })
+        .catch(err => console.log('error', err))
 })
 
 module.exports = router
--- routes/game.routes.js
+++ routes/game.routes.js
@@ -4,30 +4,66 @@ const router = express.Router()
 const IMDbApp = require('../services/api-handler')
 const IMDb = new IMDbApp
 
+const Game = require('./../models/game.model')
+
+//Variable global
+let totalGameSearchResults = {
+    start: [],
+    end: []
+}
+
 //Search Actors
-router.post('/actor/', (req, res) => {
+router.post('/actor', (req, res) => {
     const actorToSearch = req.body.name
     const side = req.body.side
     IMDb
         .searchActors(actorToSearch)
         .then(response => {
             const { data } = response
-            console.log({
-                side: side,
-                searchResults: data.results
-            })
-            res.render('pages/game/index', {
-                side: side,
-                searchResults: data.results
-            })
+            switch (side) {
+                case 'start':
+                    totalGameSearchResults.start = data.results
+                    break
+                case 'end':
+                    totalGameSearchResults.end = data.results
+                    break
+            }
+            res.render('pages/game/index', totalGameSearchResults)
         })
         .catch(err => console.timeLog(err))
 })
 
-
-
-const IMDbAPI_Key = process.env.IMDbAPI_Key
-const urlStart = 'https://imdb-api.com/en/API/'
+//
+router.get('/actor/:actorId/:side', (req, res) => {
+    const actorId = req.params.actorId
+    const side = req.params.side
+    console.log(totalGameSearchResults)
+    IMDb
+        .getActorById(actorId)
+        .then(response => {
+            const {
+                data
+            } = response
+            switch (side) {
+                case 'start':
+                    Game
+                        .create({ startActorId: actorId })
+                        .then(createdGame => {
+                            console.log(createdGame)
+                        })
+                    break
+                case 'end':
+                    Game
+                        .create({ endActorId: actorId })
+                        .then(createdGame => {
+                            console.log(createdGame)
+                        })
+                    break
+            }
+            res.render('pages/game/index', totalGameSearchResults)
+        })
+        .catch(err => console.timeLog(err))
+})
 
 //Search Movies
 router.post('/movie', (req, res) => {
@@ -60,13 +96,13 @@ router.post('/movie/byId/', (req, res) => {
 })
 
 //Get specific movie fullcast by ID
-router.post('/movie/fullCast', (req, res) => {
-    const movieId = req.body.movieId
-    const searchTerm = movieId
-    const action = 'FullCast'
-    const options = ''
-    const fullUrl = `${urlStart}${action}/${IMDbAPI_Key}/${searchTerm}/${options}`
-    res.redirect(fullUrl)
-})
+// router.post('/movie/fullCast', (req, res) => {
+//     const movieId = req.body.movieId
+//     const searchTerm = movieId
+//     const action = 'FullCast'
+//     const options = ''
+//     const fullUrl = `${urlStart}${action}/${IMDbAPI_Key}/${searchTerm}/${options}`
+//     res.redirect(fullUrl)
+// })
 
 module.exports = router
