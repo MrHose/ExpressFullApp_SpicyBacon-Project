b38fc1d716216ca02a9368d98cf82cb7851ba1ea
MrHose <josemiguel.uk@gmail.com>
2021-04-29 17:31:53 +0200

    css basics

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -85,4 +85,8 @@ router.post('/movie/byId/', isLoggedIn, (req, res) => {
     res.redirect('fullUrl')
 })
 
+router.get('/go', (req, res) => {
+    res.render('pages/game/gameOver')
+})
+
 module.exports = router
